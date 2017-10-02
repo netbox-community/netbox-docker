33b14758402024f9ca619b29c1776a1b4a622ba4
Christian Mäder <cma@nine.ch>
2017-10-02 11:08:50 +0200

    ✨ Build pre-released versions
    ... instead of development branches.

--- build-latest.sh
+++ build-latest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-URL_LATEST=https://api.github.com/repos/digitalocean/netbox/releases/latest
+URL_LATEST=https://api.github.com/repos/digitalocean/netbox/releases
 
-VERSION=$(curl "${URL_LATEST}" | jq -r ".tag_name")
+JQ_LATEST="group_by(.prerelease) | .[] | sort_by(.published_at) | reverse | .[0] | select(.prerelease==${PRERELEASE-false}) | .tag_name"
 
-./build.sh "${VERSION}" --push
+VERSION=$(curl "${URL_LATEST}" | jq -r "${JQ_LATEST}")
+
+./build.sh "${VERSION}" $@
+
