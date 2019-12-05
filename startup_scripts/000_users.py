927a545f41b6ca8490e12de63ca0d346043884e9
Les Begnaud <lbegnaud@radersolutions.com>
2019-12-05 09:41:11 -0600

    adjust groups and users startup scripts to allow custom codename filter

--- startup_scripts/000_users.py
+++ startup_scripts/000_users.py
@@ -25,10 +25,23 @@ with file.open('r') as stream:
         if user_details.get('api_token', 0):
           Token.objects.create(user=user, key=user_details['api_token'])
 
-        user_permissions = user_details.get('permissions', [])
-        if user_permissions:
-          user.user_permissions.clear()
-          for permission_codename in user_details.get('permissions', []):
-            for permission in Permission.objects.filter(codename=permission_codename):
-              user.user_permissions.add(permission)
-          user.save()
+        yaml_permissions = user_details.get('permissions', [])
+        permission_object = user
+        if yaml_permissions:
+          permission_object.permissions.clear()
+          for yaml_permission in yaml_permissions:
+            if isinstance(yaml_permission,dict):
+              # assume this is the specific codename filter function instead of an exact codename
+              permission_codename_function = list(yaml_permission.keys())[0]
+              permission_codenames = yaml_permission[permission_codename_function]
+            else:
+              permission_codename_function = 'codename'
+              permission_codenames = list({yaml_permission})
+
+            # supports either one codename from the permissions list, or multiple codenames in a codename_function dict
+            for permission_codename in permission_codenames:
+              # supports non-unique permission codenames
+              for permission in eval('Permission.objects.filter(' + permission_codename_function + '=permission_codename)'):
+                permission_object.permissions.add(permission)
+
+          permission_object.save()
