94509f86d799b718edc166a4369a7de2a9b1164e
ryanmerolle <ryan.merolle@gmail.com>
2020-12-30 19:11:09 -0500

    added route_targets startup_script

---
+++ startup_scripts/175_route_targets.py
@@ -0,0 +1,31 @@
+import sys
+
+from ipam.models import RouteTarget
+from startup_script_utils import *
+from tenancy.models import Tenant
+
+route_targets = load_yaml('/opt/netbox/initializers/route_targets.yml')
+
+if route_targets is None:
+  sys.exit()
+
+optional_assocs = {
+  'tenant': (Tenant, 'name')
+}
+
+for params in route_targets:
+  custom_field_data = pop_custom_fields(params)
+
+  for assoc, details in optional_assocs.items():
+    if assoc in params:
+      model, field = details
+      query = { field: params.pop(assoc) }
+
+      params[assoc] = model.objects.get(**query)
+
+  route_target, created = RouteTarget.objects.get_or_create(**params)
+
+  if created:
+    set_custom_fields_values(route_target, custom_field_data)
+
+    print("🎯 Created Route Target", route_target.name)
