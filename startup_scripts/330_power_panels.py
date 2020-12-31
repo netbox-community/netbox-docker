dfb0327340d370fd3ebece5d62e8cf5a61842b95
ryanmerolle <ryan.merolle@gmail.com>
2020-12-30 19:10:30 -0500

    Added power startup_scripts

---
+++ startup_scripts/330_power_panels.py
@@ -0,0 +1,41 @@
+import sys
+
+from dcim.models import Site, RackGroup, PowerPanel
+from startup_script_utils import *
+from tenancy.models import Tenant
+
+power_panels = load_yaml('/opt/netbox/initializers/power_panels.yml')
+
+if power_panels is None:
+  sys.exit()
+
+required_assocs = {
+  'site': (Site, 'name')
+}
+
+optional_assocs = {
+  'rack_group': (RackGroup, 'name')
+}
+
+for params in power_panels:
+  custom_field_data = pop_custom_fields(params)
+
+  for assoc, details in required_assocs.items():
+    model, field = details
+    query = { field: params.pop(assoc) }
+
+    params[assoc] = model.objects.get(**query)
+
+  for assoc, details in optional_assocs.items():
+    if assoc in params:
+      model, field = details
+      query = { field: params.pop(assoc) }
+
+      params[assoc] = model.objects.get(**query)
+
+  power_panel, created = PowerPanel.objects.get_or_create(**params)
+
+  if created:
+    set_custom_fields_values(power_panel, custom_field_data)
+
+    print("⚡ Created Power Panel", power_panel.site, power_panel.name)
---
+++ startup_scripts/340_power_feeds.py
@@ -0,0 +1,41 @@
+import sys
+
+from dcim.models import Rack, PowerPanel, PowerFeed
+from startup_script_utils import *
+from tenancy.models import Tenant
+
+power_feeds = load_yaml('/opt/netbox/initializers/power_feeds.yml')
+
+if power_feeds is None:
+  sys.exit()
+
+required_assocs = {
+  'power_panel': (PowerPanel, 'name')
+}
+
+optional_assocs = {
+  'rack': (Rack, 'name')
+}
+
+for params in power_feeds:
+  custom_field_data = pop_custom_fields(params)
+
+  for assoc, details in required_assocs.items():
+    model, field = details
+    query = { field: params.pop(assoc) }
+
+    params[assoc] = model.objects.get(**query)
+
+  for assoc, details in optional_assocs.items():
+    if assoc in params:
+      model, field = details
+      query = { field: params.pop(assoc) }
+
+      params[assoc] = model.objects.get(**query)
+
+  power_feed, created = PowerFeed.objects.get_or_create(**params)
+
+  if created:
+    set_custom_fields_values(power_feed, custom_field_data)
+
+    print("⚡ Created Power Feed", power_feed.name)
