2372c1eeff14e6a511acf7c9ce880bdc4f865720
ryanmerolle <ryan.merolle@gmail.com>
2021-01-15 15:02:01 -0500

    remove site from power_feeds

--- startup_scripts/340_power_feeds.py
+++ startup_scripts/340_power_feeds.py
@@ -1,6 +1,6 @@
 import sys
 
-from dcim.models import Rack, PowerPanel, PowerFeed, Site
+from dcim.models import Rack, PowerPanel, PowerFeed
 from startup_script_utils import *
 from tenancy.models import Tenant
 
@@ -10,8 +10,7 @@ if power_feeds is None:
   sys.exit()
 
 required_assocs = {
-  'power_panel': (PowerPanel, 'name'),
-  'site': (Site, 'name')
+  'power_panel': (PowerPanel, 'name')
 }
 
 optional_assocs = {
