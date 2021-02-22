0dec4d8df4a044f08d5e7ca6b75cb51562fc1785
Ryan Merolle <ryanmerolle@users.noreply.github.com>
2021-02-22 16:00:08 -0500

    Add extra.py example for ldap config

---
+++ configuration/ldap/extra.py
@@ -0,0 +1,8 @@
+####
+## This file contains extra configuration options that can't be configured
+## directly through environment variables.
+####
+
+# import ldap
+
+# AUTH_LDAP_MIRROR_GROUPS = ["netbox-group-1", "netbox-group-2"]
