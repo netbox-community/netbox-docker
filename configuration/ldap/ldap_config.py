6f4d025cb7a859c294c2d4b75f8b4680f3c99c40
ryanmerolle <ryan.merolle@gmail.com>
2021-04-20 03:38:20 -0400

    backed out ldap_config changes

--- configuration/ldap/ldap_config.py
+++ configuration/ldap/ldap_config.py
@@ -77,8 +77,7 @@ if AUTH_LDAP_REQUIRE_GROUP is not None:
 
 # For more granular permissions, we can map LDAP groups to Django groups.
 AUTH_LDAP_FIND_GROUP_PERMS = environ.get('AUTH_LDAP_FIND_GROUP_PERMS', 'True').lower() == 'true'
-if environ.get('AUTH_LDAP_MIRROR_GROUPS') is not None:
-    AUTH_LDAP_MIRROR_GROUPS = environ.get('AUTH_LDAP_MIRROR_GROUPS', '').lower() == 'true'
+AUTH_LDAP_MIRROR_GROUPS = environ.get('AUTH_LDAP_MIRROR_GROUPS', '').lower() == 'true'
 
 # Cache groups for one hour to reduce LDAP traffic
 AUTH_LDAP_CACHE_TIMEOUT = int(environ.get('AUTH_LDAP_CACHE_TIMEOUT', 3600))
