eb5cc88b25f7ee4bd7c2faa149f0c9664413e56d
ryanmerolle <ryan.merolle@gmail.com>
2021-04-20 07:46:29 -0400

    Add expample plugin configuration

---
+++ configuration/plugins.py
@@ -0,0 +1,10 @@
+# Add your plugins and plugin settings here.
+# Of course uncomment this file out.
+
+# PLUGINS = ["netbox_onboarding"]
+
+# PLUGINS_CONFIG = {
+#   "netbox_onboarding": {
+#     ADD YOUR SETTINGS HERE
+#   }
+# }
