9fab9a3434979468e1a4492df96e0ee94cd099f5
ryanmerolle <ryan.merolle@gmail.com>
2021-04-20 07:52:08 -0400

    mention plugin wiki page in plugins.py comment

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,6 +1,9 @@
 # Add your plugins and plugin settings here.
 # Of course uncomment this file out.
 
+# To learn how to build images with your required plugins
+# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+
 # PLUGINS = ["netbox_onboarding"]
 
 # PLUGINS_CONFIG = {
