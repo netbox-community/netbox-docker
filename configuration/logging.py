96545135cba17169f20aafe56a4278f1216aaf2d
ryanmerolle <ryan.merolle@gmail.com>
2021-04-25 16:38:03 -0400

    remove example evn var for logging

--- configuration/logging.py
+++ configuration/logging.py
@@ -1,4 +1,5 @@
-##Remove first comment(#) on each line to implement this working logging example.
+## Remove first comment(#) on each line to implement this working logging example.
+## Add LOGLEVEL environment variable to netbox if you use this example & want a different log level.
 #from os import environ
 #
 ## Set LOGLEVEL in netbox.env or docker-compose.overide.yml to override a logging level of INFO.
