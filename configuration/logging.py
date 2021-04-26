f924e76adfe597a307785124d86147f1bee436b1
Christian Mäder <mail@cimnine.ch>
2021-04-26 08:55:22 +0200

    Formatting

--- configuration/logging.py
+++ configuration/logging.py
@@ -1,12 +1,12 @@
-## Remove first comment(#) on each line to implement this working logging example.
-## Add LOGLEVEL environment variable to netbox if you use this example & want a different log level.
-#from os import environ
-#
-## Set LOGLEVEL in netbox.env or docker-compose.overide.yml to override a logging level of INFO.
-#LOGLEVEL = environ.get('LOGLEVEL', 'INFO')
-#
-#LOGGING = {
-#
+# # Remove first comment(#) on each line to implement this working logging example.
+# # Add LOGLEVEL environment variable to netbox if you use this example & want a different log level.
+# from os import environ
+
+# # Set LOGLEVEL in netbox.env or docker-compose.overide.yml to override a logging level of INFO.
+# LOGLEVEL = environ.get('LOGLEVEL', 'INFO')
+
+# LOGGING = {
+
 #    'version': 1,
 #    'disable_existing_loggers': False,
 #    'formatters': {
@@ -52,4 +52,4 @@
 #            'level': LOGLEVEL,
 #        }
 #    }
-#}
+# }
