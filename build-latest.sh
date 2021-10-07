ab41eaa5d9246d693c99263302b07ae704bc4d51
rsp2k <rpm@malloys.us>
2021-10-07 11:12:35 -0600

    Add friendly message/exit when jq isn't available

--- build-latest.sh
+++ build-latest.sh
@@ -6,9 +6,8 @@ echo "▶️ $0 $*"
 ###
 # Check for the jq library needed for parsing JSON
 ###
-if [ ! command -v jq &> /dev/null ]
-then
-    echo "⚠️ PRERELEASE must be either unset, 'true' or 'false', but was '${PRERELEASE}'!"
+if ! command -v jq; then
+    echo "⚠️  jq command missing from \$PATH!"
     exit 1
 fi
 
