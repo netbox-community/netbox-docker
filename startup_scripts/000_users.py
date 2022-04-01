3cf34cae73657ada2f3acf571520e5a261e82f50
Kurach Alexander <sundelor@gmail.com>
2022-04-01 12:51:05 +0300

    changed default values
    changed default values to First and Last name and is_active field

--- startup_scripts/000_users.py
+++ startup_scripts/000_users.py
@@ -13,11 +13,11 @@ for username, user_details in users.items():
         user = User.objects.create_user(
             username=username,
             password=user_details.get("password", 0) or User.objects.make_random_password(),
-            first_name=user_details.get("first_name", "no-name"),
-            last_name=user_details.get("last_name", "no-lastname"),
+            first_name=user_details.get("first_name", ""),
+            last_name=user_details.get("last_name", ""),
             email=user_details.get("email", "none"),
             is_staff=user_details.get("is_staff", False),
-            is_active=user_details.get("is_active", False),
+            is_active=user_details.get("is_active", True),
             is_superuser=user_details.get("is_superuser", False),
         )
 
