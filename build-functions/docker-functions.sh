cee1b5b079054f8e73ee0fda399c40942f844b78
Tobias Genannt <tobias.genannt@gmail.com>
2022-06-15 08:22:11 +0200

    Build ARM64 images

--- build-functions/docker-functions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-push_image_to_registry() {
-  local target_tag=$1
-  echo "⏫ Pushing '${target_tag}'"
-  $DRY docker push "${target_tag}"
-  echo "✅ Finished pushing the Docker image '${target_tag}'."
-}
