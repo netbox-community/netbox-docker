b6c53fa09eb1a6fa1440954606cc3699cf0c3cb5
hakasapl <hakansaplakog@gmail.com>
2022-07-05 12:15:59 -0400

    added netbox topology plugin

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -192,7 +192,7 @@ NAPALM_ARGS = {}
 PAGINATE_COUNT = int(environ.get('PAGINATE_COUNT', 50))
 
 # Enable installed plugins. Add the name of each plugin to the list.
-PLUGINS = []
+PLUGINS = [ "netbox_topology_views" ]
 
 # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
