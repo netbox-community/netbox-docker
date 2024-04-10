a35bce8d93351a3214fa169716a090b0b4279deb
Ethan <sk18181@bristol.ac.uk>
2024-04-10 15:58:20 +0100

    add netbox_routing netbox_dns netbox_secrets netbox_inventory

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,7 +4,7 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-PLUGINS = ["netbox_topology_views"]
+PLUGINS = ["netbox_topology_views","netbox_routing","netbox_dns","netbox_secrets","netbox_inventory"]
 
 # PLUGINS_CONFIG = {
 #   "netbox_bgp": {
