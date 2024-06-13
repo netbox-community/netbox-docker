8179050f375d049bd4fc2fa6752b923a0800db4d
Jakub Krkoška <jakub.krkoska@invia.sk>
2024-06-13 16:14:15 +0200

    feat: added support for netbox_secrets plugin

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,7 +4,7 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+PLUGINS = ["netbox_secrets"]
 
 # PLUGINS_CONFIG = {
 #   "netbox_bgp": {
