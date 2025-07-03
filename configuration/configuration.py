eaad696666bf6a713907a0672c0e54a4d2ec8507
Skye A. Fugate <skye.a.fugate@gmail.com>
2025-07-03 13:12:52 -0500

    feat: add SSO environment variable support for OKTA and Google OAuth2
    Add native support for SSO configuration through environment variables
    and Docker secrets, eliminating the need to modify configuration.py
    for common SSO providers.
    
    Changes:
    - Add OKTA OpenID Connect configuration variables:
      - SOCIAL_AUTH_OKTA_OPENIDCONNECT_KEY (env var)
      - SOCIAL_AUTH_OKTA_OPENIDCONNECT_SECRET (env var + Docker secret: okta_openidconnect_secret)
      - SOCIAL_AUTH_OKTA_OPENIDCONNECT_API_URL (env var)
    
    - Add Google OAuth2 configuration variables:
      - SOCIAL_AUTH_GOOGLE_OAUTH2_KEY (env var)
      - SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET (env var + Docker secret: google_oauth2_secret)
    
    Follows existing patterns with _read_secret() for sensitive data and
    environ.get() for non-sensitive configuration.
    
    Resolves: netbox-community/netbox-docker#1139

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -303,6 +303,12 @@ REMOTE_AUTH_SUPERUSER_GROUPS = _environ_get_and_map('REMOTE_AUTH_SUPERUSER_GROUP
 REMOTE_AUTH_SUPERUSERS = _environ_get_and_map('REMOTE_AUTH_SUPERUSERS', '', _AS_LIST)
 REMOTE_AUTH_STAFF_GROUPS = _environ_get_and_map('REMOTE_AUTH_STAFF_GROUPS', '', _AS_LIST)
 REMOTE_AUTH_STAFF_USERS = _environ_get_and_map('REMOTE_AUTH_STAFF_USERS', '', _AS_LIST)
+# SSO Configuration
+SOCIAL_AUTH_OKTA_OPENIDCONNECT_KEY = environ.get('SOCIAL_AUTH_OKTA_OPENIDCONNECT_KEY')
+SOCIAL_AUTH_OKTA_OPENIDCONNECT_SECRET = _read_secret('okta_openidconnect_secret', environ.get('SOCIAL_AUTH_OKTA_OPENIDCONNECT_SECRET', ''))
+SOCIAL_AUTH_OKTA_OPENIDCONNECT_API_URL = environ.get('SOCIAL_AUTH_OKTA_OPENIDCONNECT_API_URL')
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = _read_secret('google_oauth2_secret', environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', ''))
 
 # This repository is used to check whether there is a new release of NetBox available. Set to None to disable the
 # version check or use the URL below to check for release in the official NetBox repository.
