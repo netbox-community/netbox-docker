333924e9703c566005b788b42a11639b0f5dcf69
bboerni2 <93215657+bboerni2@users.noreply.github.com>
2025-09-26 11:53:16 +0200

    Add proxbox API service and update plugin requirements; configure environment variables

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,6 +4,22 @@ PLUGINS = [
     "netbox_prometheus_sd",
     "netbox_qrcode",
     "netbox_floorplan",
+    "netbox_proxbox",
+    "netbox_otp",
+    "netbox_lists",
+    "netbox_inventory",
+    "netbox_dns",
+    "netbox_reorder_rack",
 ]
 
-PLUGINS_CONFIG = {}
+PLUGINS_CONFIG = {
+    "netbox_proxbox": {
+    },
+    "netbox_inventory": {
+        "sync_serial_to_device": True,
+        "sync_asset_tag_to_device": True,
+    },
+    "netbox_otp": {
+        # "enforce_otp": False,  # ggf. global erzwingen
+    },
+}
