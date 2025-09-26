9e7ac3610e6dd99d434ef3df6fe316ad4ab3d104
bboerni2 <93215657+bboerni2@users.noreply.github.com>
2025-09-26 08:10:37 +0200

    Refactor plugins.py to remove commented-out code and simplify PLUGINS_CONFIG; update netbox.env to ensure DB_WAIT_DEBUG is set

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,18 +1,3 @@
-# Add your plugins and plugin settings here.
-# Of course uncomment this file out.
-
-# To learn how to build images with your required plugins
-# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
-
-# PLUGINS = ["netbox_bgp"]
-
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
-
-
 PLUGINS = [
     "netbox_topology_views",
     "netbox_lifecycle",
@@ -20,9 +5,4 @@ PLUGINS = [
     "netbox_qrcode",
 ]
 
-PLUGINS_CONFIG = {
-    # Beispiel für Topology Views, falls nötig:
-    # "netbox_topology_views": {
-    #     "some_setting": "value"
-    # }
-}
+PLUGINS_CONFIG = {}
