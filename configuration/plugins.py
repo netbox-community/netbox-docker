c63a3feff5f77c60742a48441ca101210a312f0b
bboerni2 <93215657+bboerni2@users.noreply.github.com>
2025-09-29 07:55:59 +0200

    Remove unused plugins and tidy up requirements-plugins.txt

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,22 +1,16 @@
 PLUGINS = [
     "netbox_topology_views",
     "netbox_lifecycle",
-    "netbox_prometheus_sd",
-    "netbox_qrcode",
     "netbox_floorplan",
-    "netbox_proxbox",
     "netbox_lists",
     "netbox_inventory",
-    "netbox_dns",
     "netbox_reorder_rack",
+    # "netbox_proxbox",
 ]
 
 PLUGINS_CONFIG = {
     "netbox_inventory": {
         "sync_serial_to_device": True,
         "sync_asset_tag_to_device": True,
-    },
-    "netbox_otp": {
-        # "enforce_otp": False,  # ggf. global erzwingen
     },
 }
