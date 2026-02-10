70ecbdbaac9e4f44fde164918d7c2ae814b4d35e
OMGhast <nkpbon@gmail.com>
2026-02-10 17:50:25 -0600

    Update configuration.py - COPILOT_ENABLED

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -363,3 +363,6 @@ TIME_ZONE = environ.get('TIME_ZONE', 'UTC')
 
 # If true disables miscellaneous functionality which depends on access to the Internet.
 ISOLATED_DEPLOYMENT = _environ_get_and_map('ISOLATED_DEPLOYMENT', 'False', _AS_BOOL)
+
+# Enables or disables the NetBox Copilot agent globally. When enabled, users can opt to toggle the agent individually.
+COPILOT_ENABLED = _environ_get_and_map('COPILOT_ENABLED', 'True', _AS_BOOL
