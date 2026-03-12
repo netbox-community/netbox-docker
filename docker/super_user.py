9ba7f3dce33d2185751fd812419b50af568d6a77
Eduardo Pozo <edupoz@sykehuspartner.no>
2026-03-12 19:49:15 +0100

    Add SUPERUSER_API_KEY, fix token message

--- docker/super_user.py
+++ docker/super_user.py
@@ -19,9 +19,15 @@ def _read_secret(secret_name: str, default: str | None = None) -> str | None:
 su_name = environ.get("SUPERUSER_NAME", "admin")
 su_email = environ.get("SUPERUSER_EMAIL", "admin@example.com")
 su_password = _read_secret("superuser_password", environ.get("SUPERUSER_PASSWORD", "admin"))
+if not environ.get('SUPERUSER_API_TOKEN'):
+    print('⚠️ Warning: Defaulting to the old default admin token. This token is widely known; please remove it.')
 su_api_token = _read_secret(
     "superuser_api_token",
     environ.get("SUPERUSER_API_TOKEN", "0123456789abcdef0123456789abcdef01234567"),
+)
+su_api_key = _read_secret(
+    'superuser_api_key',
+    environ.get('SUPERUSER_API_KEY', 'cae3ju9jeaqu'),
 )
 
 if not User.objects.filter(username=su_name):
@@ -31,6 +37,12 @@ if not User.objects.filter(username=su_name):
         print("⚠️ No API token will be created as API_TOKEN_PEPPERS is not set")
         msg = f"💡 Superuser Username: {su_name}, E-Mail: {su_email}"
     else:
-        t = Token.objects.create(user=u, token=su_api_token, version=TokenVersionChoices.V2)
-        msg = f"💡 Superuser Username: {su_name}, E-Mail: {su_email}, API Token: {t} (use with '{t.get_auth_header_prefix()}<Your token>')"
+        if su_api_key:
+            t = Token.objects.create(user=u, token=su_api_token, version=TokenVersionChoices.V2, key=su_api_key)
+        else:
+            t = Token.objects.create(user=u, token=su_api_token, version=TokenVersionChoices.V2)
+        msg = (
+            f'💡 Superuser Username: {su_name}, E-Mail: {su_email}, '
+            f"API Token: use with '{t.get_auth_header_prefix()}<Your token>'"
+        )
     print(msg)
