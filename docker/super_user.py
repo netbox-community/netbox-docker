e130d69c5bdbd0b438691a553321cb9b4cea816e
Tobias Genannt <tobias.genannt@qbeyond.de>
2026-03-16 10:47:03 +0100

    Minor changes:
    - Use default for key
    - Changed message to not print the token

--- docker/super_user.py
+++ docker/super_user.py
@@ -19,18 +19,15 @@ def _read_secret(secret_name: str, default: str | None = None) -> str | None:
 su_name = environ.get("SUPERUSER_NAME", "admin")
 su_email = environ.get("SUPERUSER_EMAIL", "admin@example.com")
 su_password = _read_secret("superuser_password", environ.get("SUPERUSER_PASSWORD", "admin"))
-# Sets the superuser API Token, defaults to widely known default
 if not environ.get("SUPERUSER_API_TOKEN"):
-    print("⚠️ Warning: Defaulting to the old default admin token in your database. This token is widely known; please remove it.")
+    print("⚠️ Warning: Defaulting to the old default admin token. This token is widely known; please remove it.")
 su_api_token = _read_secret(
     "superuser_api_token",
     environ.get("SUPERUSER_API_TOKEN", "0123456789abcdef0123456789abcdef01234567"),
 )
-
-# Sets the superuser API key, defaults to a randomly generated key.
 su_api_key = _read_secret(
     "superuser_api_key",
-    environ.get("SUPERUSER_API_KEY"),
+    environ.get("SUPERUSER_API_KEY", "cae3ju9jeaqu"),
 )
 
 if not User.objects.filter(username=su_name):
@@ -44,5 +41,5 @@ if not User.objects.filter(username=su_name):
             t = Token.objects.create(user=u, token=su_api_token, version=TokenVersionChoices.V2, key=su_api_key)
         else:
             t = Token.objects.create(user=u, token=su_api_token, version=TokenVersionChoices.V2)
-        msg = f"💡 Superuser Username: {su_name}, E-Mail: {su_email}, API Token: {su_api_token} (use with '{t.get_auth_header_prefix()}<Your token>')"
+        msg = f"💡 Superuser Username: {su_name}, E-Mail: {su_email}, API Token: use with '{t.get_auth_header_prefix()}<Your token>'"
     print(msg)
